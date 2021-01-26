9ef4309b8f3dffd2dc1eefa5c9884353bf5fd58e
Lucy Shi <lucys@usc.edu>
2021-01-26 10:45:52 +0800

    init

--- hw4/cs285/infrastructure/rl_trainer.py
+++ hw4/cs285/infrastructure/rl_trainer.py
@@ -188,12 +188,41 @@ class RL_Trainer(object):
             envsteps_this_batch: the sum over the numbers of environment steps in paths
             train_video_paths: paths which also contain videos for visualization purposes
         """
-        # TODO: get this from Piazza
+        # TODO: get this from Piazza √
+        if itr == 0 and initial_expertdata is not None:
+            file = open(initial_expertdata, 'rb')
+            loaded_paths = pickle.load(file)
+            file.close()
+            return loaded_paths, 0, None
+
+        envsteps_this_batch = 0
+        paths = []
+        while envsteps_this_batch <= num_transitions_to_sample:
+            paths_this_batch, timesteps_this_batch = utils.sample_trajectories(self.env, collect_policy,
+                                                                               max((num_transitions_to_sample - envsteps_this_batch) // self.params['ep_len'], 1), self.params['ep_len'])
+            paths.extend(paths_this_batch)
+            envsteps_this_batch += timesteps_this_batch
+
+        train_video_paths = None
+        if save_expert_data_to_disk:
+            print('\nCollecting train rollouts to be used for saving videos...')
+            train_video_paths = utils.sample_n_trajectories(
+                self.env, collect_policy, MAX_NVIDEO, MAX_VIDEO_LEN, True)
 
         return paths, envsteps_this_batch, train_video_paths
 
     def train_agent(self):
-    # TODO: get this from Piazza
+    # TODO: get this from Piazza √
+        # print('\nTraining agent using sampled data from replay buffer...')
+        all_logs = []
+        for train_step in range(self.params['num_agent_train_steps_per_iter']):
+            ob_batch, ac_batch, re_batch, next_ob_batch, terminal_batch \
+                = self.agent.sample(self.params['train_batch_size'])
+
+            train_log = self.agent.train(
+                ob_batch, ac_batch, re_batch, next_ob_batch, terminal_batch)
+            all_logs.append(train_log)
+        return all_logs
 
     ####################################
     ####################################
--- hw4/cs285/infrastructure/utils.py
+++ hw4/cs285/infrastructure/utils.py
@@ -55,14 +55,62 @@ def mean_squared_error(a, b):
 ############################################
 
 def sample_trajectory(env, policy, max_path_length, render=False, render_mode=('rgb_array')):
-# TODO: get this from Piazza
+# TODO: get this from Piazza √
+    ob = env.reset()
+
+    # init vars
+    obs, acs, rewards, next_obs, terminals, image_obs = [], [], [], [], [], []
+    steps = 0
+    while True:
+
+        # render image of the simulated env
+        if render:
+            if 'rgb_array' in render_mode:
+                if hasattr(env, 'sim'):
+                    image_obs.append(env.sim.render(
+                        camera_name='track', height=500, width=500)[::-1])
+                else:
+                    image_obs.append(env.render(mode=render_mode))
+            if 'human' in render_mode:
+                env.render(mode=render_mode)
+                time.sleep(env.model.opt.timestep)
+
+        # use the most recent ob to decide what to do
+        obs.append(ob)
+        ac = policy.get_action(ob)
+        ac = ac[0]
+        acs.append(ac)
+
+        # take that action and record results
+        ob, rew, done, _ = env.step(ac)
+
+        # record result of taking that action
+        steps += 1
+        next_obs.append(ob)
+        rewards.append(rew)
+
+        # end the rollout if the rollout ended
+        rollout_done = 1 if steps >= max_path_length or done else 0
+        terminals.append(rollout_done)
+
+        if rollout_done:
+            break
+
+    return Path(obs, image_obs, acs, rewards, next_obs, terminals)
 
 def sample_trajectories(env, policy, min_timesteps_per_batch, max_path_length, render=False, render_mode=('rgb_array')):
     """
         Collect rollouts using policy
         until we have collected min_timesteps_per_batch steps
     """
-    # TODO: get this from Piazza
+    # TODO: get this from Piazza √
+    timesteps_this_batch = 0
+    paths = []
+    while timesteps_this_batch < min_timesteps_per_batch:
+        path = sample_trajectory(
+            env, policy, max_path_length, render, render_mode)
+        paths.append(path)
+        timesteps_this_batch += get_pathlength(path)
 
     return paths, timesteps_this_batch
 
@@ -70,7 +118,13 @@ def sample_n_trajectories(env, policy, ntraj, max_path_length, render=False, ren
     """
         Collect ntraj rollouts using policy
     """
-    # TODO: get this from Piazza
+    # TODO: get this from Piazza √
+    paths = []
+
+    for _ in range(ntraj):
+        path = sample_trajectory(
+            env, policy, max_path_length, render, render_mode)
+        paths.append(path)
 
     return paths
 
