de85493b07e3f294d35a9598b31d827485d92bde
Lucy Shi <lucys@usc.edu>
2021-01-27 15:48:01 +0800

    problem 1

--- hw4/cs285/agents/mb_agent.py
+++ hw4/cs285/agents/mb_agent.py
@@ -40,19 +40,21 @@ class MBAgent(BaseAgent):
         # NOTE: each model in the ensemble is trained on a different random batch of size batch_size
         losses = []
         num_data = ob_no.shape[0]
-        num_data_per_ens = int(num_data / self.ensemble_size)
+        num_data_per_env = int(num_data / self.ensemble_size)
 
         for i in range(self.ensemble_size):
 
             # select which datapoints to use for this model of the ensemble
             # you might find the num_data_per_env variable defined above useful
 
-            observations = # TODO(Q1)
-            actions = # TODO(Q1)
-            next_observations = # TODO(Q1)
+            # observations = # TODO(Q1)
+            # actions = # TODO(Q1)
+            # next_observations = # TODO(Q1)
+            observations, actions, _, next_observations, _ = self.sample(num_data_per_env)
 
             # use datapoints to update one of the dyn_models
-            model =  # TODO(Q1)
+            # model =  # TODO(Q1)
+            model = dyn_models[i]
             log = model.update(observations, actions, next_observations,
                                 self.data_statistics)
             loss = log['Training Loss']
@@ -87,4 +89,5 @@ class MBAgent(BaseAgent):
         # NOTE: sampling batch_size * ensemble_size,
         # so each model in our ensemble can get trained on batch_size data
         return self.replay_buffer.sample_random_data(
-            batch_size * self.ensemble_size)
+            batch_size * self.ensemble_size) 
+            # ?
